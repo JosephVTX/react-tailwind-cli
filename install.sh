fea48a579b9de00dc1f9f54174861c298a0a493a
Joseph Vega Callupe <91026290+JosephVTX@users.noreply.github.com>
2022-10-30 00:00:59 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Ingrese el nombre de tu nuevo proyecto: "; read pn
+
+pnpm create vite --template react $pn
+
+cd "$pn"
+
+pnpm install tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
+
+rm tailwind.config.cjs
+
+echo "/** @type {import('tailwindcss').Config} */" >> tailwind.config.cjs
+echo "module.exports = {" >> tailwind.config.cjs
+echo "  content: [" >> tailwind.config.cjs
+echo '    "./index.html",' >> tailwind.config.cjs
+echo '    "./src/**/*.{js,ts,jsx,tsx}",' >> tailwind.config.cjs
+echo "  ]," >> tailwind.config.cjs
+echo "  theme: {" >> tailwind.config.cjs
+echo "    extend: {}," >> tailwind.config.cjs
+echo "  }," >> tailwind.config.cjs
+echo "  plugins: []," >> tailwind.config.cjs
+echo "}" >> tailwind.config.cjs
+
+rm src/index.css
+
+echo "@tailwind base;" >> src/index.css
+echo "@tailwind components;" >> src/index.css
+echo "@tailwind utilities;" >> src/index.css
+
+code .
+
+pnpm install
+
