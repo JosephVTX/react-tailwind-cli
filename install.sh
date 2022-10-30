80830eda8587242b17f6de474d5960e92922b3f3
Joseph Vega Callupe <91026290+JosephVTX@users.noreply.github.com>
2022-10-30 12:36:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,15 +15,25 @@ npx tailwindcss init -p
 rm -rf tailwind.config.cjs
 
 echo "/** @type {import('tailwindcss').Config} */" >> tailwind.config.cjs
+
 echo "module.exports = {" >> tailwind.config.cjs
+
 echo "  content: [" >> tailwind.config.cjs
+
 echo '    "./index.html",' >> tailwind.config.cjs
+
 echo '    "./src/**/*.{js,ts,jsx,tsx}",' >> tailwind.config.cjs
+
 echo "  ]," >> tailwind.config.cjs
+
 echo "  theme: {" >> tailwind.config.cjs
+
 echo "    extend: {}," >> tailwind.config.cjs
+
 echo "  }," >> tailwind.config.cjs
+
 echo "  plugins: []," >> tailwind.config.cjs
+
 echo "}" >> tailwind.config.cjs
 
 rm -rf src/index.css
@@ -31,5 +41,7 @@ rm -rf src/index.css
 echo "@tailwind base;" >> src/index.css
 echo "@tailwind components;" >> src/index.css
 echo "@tailwind utilities;" >> src/index.css
+
+code .
 
 exit
